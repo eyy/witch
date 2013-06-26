33f8776099af0480d318208292a462ebb371904a
etaypere <etaypere@gmail.com>
2013-06-27 01:21:42 +0300

    add some general rivets formatters at rivets-extras.js

---
+++ dist/rivets-extras.js
@@ -0,0 +1,27 @@
+(function() {
+    var slice = [].slice;
+
+    /*
+     Rivets Formatters
+     more here: https://github.com/mikeric/rivets/wiki/Example-formatters
+     */
+    rivets.formatters.eq = function () {
+        var args = slice.call(arguments);
+        return args.indexOf(args.shift()) !== -1;
+    };
+    rivets.formatters.neq = function() {
+        var args = slice.call(arguments);
+        return args.indexOf(args.shift()) === -1;
+    };
+    rivets.formatters.pass = function(fn, arg) {
+        return function(e, el, binding) {
+            var ctx = binding.view.models;
+            fn.call(this, ctx[arg] || arg, binding);
+        };
+    };
+
+    /*
+     Rivets Binders
+     */
+
+})();
