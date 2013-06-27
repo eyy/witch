ed6c3f52a947b075fe223fc4a481db322af234fa
etaypere <etaypere@gmail.com>
2013-06-27 21:44:04 +0300

    templates and more

--- dist/witch.js
+++ dist/witch.js
@@ -13,7 +13,8 @@
     /*
      Model
      */
-    var Model = function(data) {
+    var Model = function(data, collection) {
+        this._collection = collection || this._collection;
         $.extend(this, data);
     };
     $.extend(Model.prototype, {
@@ -71,7 +72,7 @@
             url = model;
             model = null;
         }
-        this.model = model || Model;
+        this.model = model || this.model || Model;
         this.url = url || this.url;
 
         this.list = [];
@@ -88,10 +89,10 @@
             if (Array.isArray(model)) {
                 model.forEach(this.push.bind(this));
                 this.filled = true;
-                return;
+                return false;
             }
 
-            model = (model instanceof Model) ? model : new this.model(model);
+            model = (model instanceof Model) ? model : new this.model(model, this);
             model._collection = this;
             if (this.filled)
                 model._new = true;
@@ -115,16 +116,17 @@
     /*
      Template
      */
-    var View = function(data, template) {
+    var Template = function(data, template) {
         this.template || (this.template = template);
         this.data = $.extend({}, this.data, data);
         this.data.tpl = this;
     };
-    $.extend(View.prototype, {
+    $.extend(Template.prototype, {
         render: function(fn) {
             if (!this.template)
                 return console.error('No template', this);
 
+            fn || (fn = this.ready);
             witch.config.template(this.template, function(err, el) {
                 this.el = el;
                 this.binding = rivets.bind(this.el, this.data);
@@ -138,13 +140,39 @@
     window.witch = {
         Model: Model,
         Collection: Collection,
-        View: View,
+        Template: Template,
         config: {
             template: function(tpl, cb) {
-                var el = $('[data-template="' + tpl + '"] :first:not(:empty)').clone();
+                var el = $('[data-template="' + tpl + '"] > :not(:empty)').clone();
                 cb(!el.length, el);
-            }
+            },
+            preload: true
+        },
+        init: function() {
+            $('[data-rivets]').each(function() {
+                var t = $(this),
+                    m = window[t.data('rivets')];
+                if (m)
+                    rivets.bind(t, m);
+            });
+        },
+        inherit: function() {
+            var args = slice.call(arguments),
+                parent = args[0],
+                Class = function() {
+                    parent.apply(this, arguments);
+                };
+            args.unshift(Class);
+            $.extend.apply(this, args.map(function(c) {
+                return (typeof c == 'function') ? c.prototype : c;
+            }));
+            return Class;
         }
     };
+
+    $(function() {
+        if (witch.config.preload)
+            witch.init();
+    })
 
 })();
